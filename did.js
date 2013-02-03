65b7d2cadcb2ed79a5ffecc6ae497db4724e1f6c
max <maxrichman@gmail.com>
2013-02-03 15:58:42 -0500

    added json button

--- did.js
+++ did.js
@@ -8,8 +8,9 @@ json = {
             "projectName":"USAID Geographic Information Specialist",
             "projectDescription":"Introduced custom mapping services and grew team to six people at USAID's Office of Transition Initiatives",
             "duration":48,
-            "factor":300,
+            "factor":180,
             "show":true,
+            "url":"http://transition.usaid.gov/our_work/cross-cutting_programs/transition_initiatives/aboutoti.html",
             "img1":"http://datahh.org/images/oti450.jpg",
             "img2":"http://datahh.org/images/oti450a.jpg"
             },
@@ -20,8 +21,32 @@ json = {
             "duration":4,
             "factor":200,
             "show":true,
+            "url":"http://aseanpostnargiskm.org/",
             "img1":"http://datahh.org/images/asean450.jpg",
             "img2":"http://datahh.org/images/asean450b.jpg"
             },
+            {
+            "id":2,
+            "projectName":"ASEAN Humanitarian Survey Data Manager",
+            "projectDescription":"Managed data collection for post-disaster survey in Myanmar (Burma)",
+            "duration":4,
+            "factor":140,
+            "show":true,
+            "url":"http://aseanpostnargiskm.org/",
+            "img1":"http://datahh.org/images/asean450.jpg",
+            "img2":"http://datahh.org/images/asean450b.jpg"
+            },
+            {
+            "id":1,
+            "projectName":"USAID Geographic Information Specialist",
+            "projectDescription":"Introduced custom mapping services and grew team to six people at USAID's Office of Transition Initiatives",
+            "duration":48,
+            "factor":400,
+            "show":true,
+            "url":"http://transition.usaid.gov/our_work/cross-cutting_programs/transition_initiatives/aboutoti.html",
+            "img1":"http://datahh.org/images/oti450.jpg",
+            "img2":"http://datahh.org/images/oti450a.jpg"
+            },
+
         ]
 };
