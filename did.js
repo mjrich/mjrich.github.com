280492a8851da58d3242257d91d6edb8d77504c8
max <maxrichman@gmail.com>
2013-02-13 10:14:23 -0500

    updated styling based on old website theme

--- did.js
+++ did.js
@@ -2,40 +2,15 @@ json = {
 "name":"Max Richman",
 "property":"datahh",
 "tag-line":"data products for international development",
+"url1":"https://twitter.com/richmanmax",
+"url1-name":"Twitter",
+"url2":"https://github.com/mjrich",
+"url2-name":"GitHub",
+"url3":"https://scraperwiki.com/profiles/mjrich/",
+"url3-name":"ScraperWiki",
+"url4":"http://www.linkedin.com/in/maxrichman",
+"url4-name":"LinkedIn",
 "did": [
-            {
-            "id":1,
-            "projectName":"USAID Geographic Information Specialist",
-            "projectDescription":"Introduced custom mapping services and grew team to six people at USAID's Office of Transition Initiatives",
-            "duration":48,
-            "factor":180,
-            "show":true,
-            "url":"http://transition.usaid.gov/our_work/cross-cutting_programs/transition_initiatives/aboutoti.html",
-            "img1":"http://datahh.org/images/oti450.jpg",
-            "img2":"http://datahh.org/images/oti450a.jpg"
-            },
-            {
-            "id":2,
-            "projectName":"ASEAN Humanitarian Survey Data Manager",
-            "projectDescription":"Managed data collection for post-disaster survey in Myanmar (Burma)",
-            "duration":4,
-            "factor":200,
-            "show":true,
-            "url":"http://aseanpostnargiskm.org/",
-            "img1":"http://datahh.org/images/asean450.jpg",
-            "img2":"http://datahh.org/images/asean450b.jpg"
-            },
-            {
-            "id":2,
-            "projectName":"ASEAN Humanitarian Survey Data Manager",
-            "projectDescription":"Managed data collection for post-disaster survey in Myanmar (Burma)",
-            "duration":4,
-            "factor":140,
-            "show":true,
-            "url":"http://aseanpostnargiskm.org/",
-            "img1":"http://datahh.org/images/asean450.jpg",
-            "img2":"http://datahh.org/images/asean450b.jpg"
-            },
             {
             "id":1,
             "projectName":"USAID Geographic Information Specialist",
@@ -46,6 +21,39 @@ json = {
             "url":"http://transition.usaid.gov/our_work/cross-cutting_programs/transition_initiatives/aboutoti.html",
             "img1":"http://datahh.org/images/oti450.jpg",
             "img2":"http://datahh.org/images/oti450a.jpg"
+            },
+            {
+            "id":2,
+            "projectName":"ASEAN Humanitarian Survey Data Manager",
+            "projectDescription":"Managed data collection for post-disaster survey in Myanmar (Burma)",
+            "duration":4,
+            "factor":300,
+            "show":true,
+            "url":"http://aseanpostnargiskm.org/",
+            "img1":"http://datahh.org/images/asean450.jpg",
+            "img2":"http://datahh.org/images/asean450b.jpg"
+            },
+            {
+            "id":3,
+            "projectName":"LSE Graduate Dissertation",
+            "projectDescription":"Understanding post-disaster migration patterns in Myanmar",
+            "duration":4,
+            "factor":200,
+            "show":true,
+            "url":"http://dl.dropbox.com/u/15148850/Richman%20MSc%20SRM%20Dissertation%202011-08-19.pdf",
+            "img1":"http://datahh.org/images/msc450.jpg",
+            "img2":"http://datahh.org/images/msc450b.jpg"
+            },
+            {
+            "id":4,
+            "projectName":"DataKind Volunteer with DC Action for Children",
+            "projectDescription":"Built interactive web map for local DC charity with fellow volunteers",
+            "duration":48,
+            "factor":200,
+            "show":true,
+            "url":"http://www.dcactionforchildren.org/kids-count/dc-kids-count-data-tools",
+            "img1":"datakindwork.png",
+            "img2":"datakindmap.png"
             },
 
         ]
