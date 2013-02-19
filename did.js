fbdc5182b450130458a29fc5ba580e7c78a6591e
max <maxrichman@gmail.com>
2013-02-18 22:43:48 -0500

    implemented Fisher-Yates shuffle algorithm as well as randomization between two images. added a few more did items to the json

--- did.js
+++ did.js
@@ -10,73 +10,118 @@ json = {
 "url3-name":"ScraperWiki",
 "url4":"http://www.linkedin.com/in/maxrichman",
 "url4-name":"LinkedIn",
-"did": [
+"did":[
             {
             "id":1,
-            "projectName":"USAID Geographic Information Specialist",
+            "projectName":"USAID - Geographic Information Specialist",
             "projectDescription":"Introduced custom mapping services and grew team to six people at USAID's Office of Transition Initiatives",
             "duration":48,
             "factor":250,
             "show":true,
             "url":"http://transition.usaid.gov/our_work/cross-cutting_programs/transition_initiatives/aboutoti.html",
-            "img1":"images/oti450.jpg",
-            "img2":"images/oti450a.jpg"
+            "imgs":{"1":"images/oti450.jpg","2":"images/oti450a.jpg"}
             },
             {
             "id":2,
-            "projectName":"ASEAN Humanitarian Survey Data Manager",
-            "projectDescription":"Managed data collection for post-disaster survey in Myanmar (Burma)",
+            "projectName":"ASEAN - Humanitarian Survey Data Manager",
+            "projectDescription":"Managed data collection for post-disaster survey in Myanmar (Burma) through iMMAP",
             "duration":4,
             "factor":250,
             "show":true,
             "url":"http://aseanpostnargiskm.org/",
-            "img1":"images/asean450.jpg",
-            "img2":"images/asean450b.jpg"
+            "imgs":{"1":"images/asean450.jpg","2":"images/asean450b.jpg"}
             },
             {
             "id":3,
-            "projectName":"LSE Graduate Dissertation",
+            "projectName":"LSE - Graduate Dissertation",
             "projectDescription":"Understanding post-disaster migration patterns in Myanmar",
             "duration":12,
             "factor":250,
             "show":true,
             "url":"http://dl.dropbox.com/u/15148850/Richman%20MSc%20SRM%20Dissertation%202011-08-19.pdf",
-            "img1":"images/msc450.jpg",
-            "img2":"images/msc450b.jpg"
+            "imgs":{"1":"images/msc450.jpg","2":"images/msc450b.jpg"}
             },
-            {
+	    {
             "id":4,
-            "projectName":"DataKind Volunteer with DC Action for Children",
+            "projectName":"DataKind - Volunteer with DC Action for Children",
             "projectDescription":"Built interactive web map for local DC charity with fellow volunteers",
             "duration":7,
             "factor":250,
             "show":true,
             "url":"http://www.dcactionforchildren.org/kids-count/dc-kids-count-data-tools",
-            "img1":"images/datakindwork.png",
-            "img2":"images/datakindmap.png"
+            "imgs":{"1":"images/datakindwork.png","2":"images/datakindmap.png"}
             },
             {
             "id":5,
-            "projectName":"Fund for Peace Horn of Africa Cartography",
+            "projectName":"Fund for Peace - Horn of Africa Cartography",
             "projectDescription":"Built maps for report on threat convergence in the horn of Africa",
             "duration":2,
             "factor":250,
             "show":true,
             "url":"http://www.fundforpeace.org/tc/images/Publications/east_africa_report.pdf",
-            "img1":"images/horn450.jpg",
-            "img2":"images/horn450a.jpg"
+            "imgs":{"1":"images/horn450.jpg","2":"images/horn450a.jpg"}
             },
             {
             "id":6,
-            "projectName":"Brazillian Deforestation Estimation",
+            "projectName":"SunOne - Brazillian Deforestation Estimation",
             "projectDescription":"Analyzed and built time series visual for REDD+ proposal",
             "duration":1,
             "factor":250,
             "show":true,
             "url":"images/Deforestation_Time_Series.gif",
-            "img1":"images/sunone450.jpg",
-            "img2":"images/Deforestation_Time_Series.gif"
+            "imgs":{"1":"images/sunone450.jpg","2":"images/Deforestation_Time_Series.gif"}
+            },
+	    {
+            "id":7,
+            "projectName":"ScraperWiki - Journalist Data Camp DC",
+            "projectDescription":"Working with AU wunderkind Zach, scraped Maryland school scores PDFs",
+            "duration":1,
+            "factor":250,
+            "show":true,
+            "url":"http://www.american.edu/americantoday/campus-news/20120420-SOC-Partners-with-Washington-Post-for-ScraperWiki-Event.cfm",
+            "imgs":{"1":"images/jdcdc1.jpg","2":"images/jdcdc2.png"}
+            },
+	    {
+            "id":8,
+            "projectName":"InterMedia - Tanzania Mobile Money Tracker",
+            "projectDescription":"Calculated Grameen PPI poverty index, divised first segmentation, QCed reports, and created maps",
+            "duration":4,
+            "factor":250,
+            "show":true,
+            "url":"http://www.audiencescapes.org/country-profiles/tanzania/tanzania/tanzania-681",
+            "imgs":{"1":"images/tmmt1.png","2":"images/tmmt2.png"}
+            },
+	    {
+            "id":9,
+            "projectName":"GeoPoll/InterMedia - Tunisia SMS Survey Pilot",
+            "projectDescription":"Questionnaire design, analyzed results with R",
+            "duration":2,
+            "factor":250,
+            "show":true,
+            "url":"https://geopoll.net/content/assessment-international-remittances-africa-tunisia-pilot",
+            "imgs":{"1":"images/geopoll1.jpg","2":"images/geopoll2.png"}
+            },
+	    {
+            "id":10,
+            "projectName":"UNICEF - Myanmar Child Friendly Schools Report",
+            "projectDescription":"With colleague Cole, cleaned and analyzed over 200 indicators of school performance in Myanmar with Stata",
+            "duration":2,
+            "factor":250,
+            "show":true,
+            "url":"http://www.unicef.org/myanmar/",
+            "imgs":{"1":"images/unicef1.png", "2":"images/unicef2.png"}
+            },
+	    {
+            "id":11,
+            "projectName":"EcoHack3 - Bugs, Bikes, and Wetlands",
+            "projectDescription":"Made network images of bugs w/ @gephi, maps of wetland developments w/ @mapbox, & DC @bikeshare stats w/ R",
+            "duration":2,
+            "factor":250,
+            "show":true,
+            "url":"http://www.pbs.org/idealab/2012/12/ecohack-highlights-sewage-pedal-power-cotton-candy353.html",
+            "imgs":{"1":"images/ecohack1.jpg","2":"images/ecohack2.jpg"}
             },
 
-        ]
-};
+
+           
+	]};
